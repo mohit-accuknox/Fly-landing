3b259d26aa33e1fe3323eebee7f9ab10c505707d
Mohit Sharma <iamxguy352@gmail.com>
2022-09-14 11:41:18 +0530

    Did some changes in js code

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
         <div className="flex items-center justify-center gap-10 ">
           <h1 className="cursor-pointer text-white text-3xl">Fly.io</h1>
         </div>
-        <ul className={isActive ? "flex items-start gap-10 lg:absolute lg:top-24 lg:bg-gray-700 transition-all sm:hidden lg:p-10 lg:-left-1/2 lg:flex-col lg:w-6/12 lg:items-center sm:w-full": "flex items-start gap-10 lg:absolute lg:top-24 lg:bg-gray-700 lg:p-10 transition-all lg:-left-2 lg:flex-col lg:w-6/12 lg:items-center sm:w-full"}>
+        <ul className={isActive ? "flex items-start gap-10 lg:absolute lg:top-24 lg:bg-gray-700 lg:p-10 transition-all lg:-left-2 lg:flex-col lg:w-6/12 lg:items-center sm:w-full" : "flex items-start gap-10 lg:absolute lg:top-24 lg:bg-gray-700 transition-all sm:hidden lg:p-10 lg:-left-1/2 lg:flex-col lg:w-6/12 lg:items-center sm:w-full"}>
           <li>
             <a
               href="#"
@@ -60,7 +60,7 @@ const Navbar = () => {
           </li>
         </ul>
         <div className="flex justify-center items-center gap-9">
-        <img src={isActive ? "images/menu.png" : "images/cross.png"} alt="" className="hidden lg:block" onClick={() => setIsActive(!isActive)}/>
+        <img src={isActive ? "images/cross.png" :  "images/menu.png" } alt="" className="hidden lg:block" onClick={() => setIsActive(!isActive)}/>
           <button className="border-2 border-white px-4 text-white rounded-lg h-10 sm:hidden transition-all hover:text-shadow-none hover:text-violet-600 hover:border-violet-200 hover:bg-violet-200">
             Sign In
           </button>
